560ac29b9f9524edf5e6b9e6da9e0022e0c9beaf
shen <1585190690@qq.com>
2017-03-15 10:19:13 +0800

    first commit

---
+++ app/src/main/java/com/example/shen/helloworld/Calculate.java
@@ -0,0 +1,19 @@
+package com.example.shen.helloworld;
+
+/**
+ * Created by shen on 2017/3/15.
+ */
+public class Calculate {
+        public double sum(double a,double b){
+            return a+b;
+        }
+        public double substract(double a,double b){
+            return a-b;
+        }
+        public double multiply(double a,double b){
+            return a*b;
+        }
+        public double divide(double a,double b){
+            return a/b;
+        }
+}
---
+++ app/src/test/java/com/example/shen/helloworld/CalculateTest.java
@@ -0,0 +1,37 @@
+package com.example.shen.helloworld;
+
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by shen on 2017/3/15.
+ */
+public class CalculateTest {
+    private Calculate calculate;
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testSum() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testSubstract() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testMultiply() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testDivide() throws Exception {
+
+    }
+}
